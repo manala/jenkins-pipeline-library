ed388493da6eb886f3830bfc470e4a2e4d91a9f1
Florian Rey <florian.rey@elao.com>
2017-02-17 14:04:00 +0100

    Fix lint css

--- vars/manalaLintCss.groovy
+++ vars/manalaLintCss.groovy
@@ -1,6 +1,6 @@
 def call(String version = 'latest', Closure body) {
   script {
-    docker.image("manala/lint-js:${version}").inside {
+    docker.image("manala/lint-css:${version}").inside {
       body()
     }
   }
